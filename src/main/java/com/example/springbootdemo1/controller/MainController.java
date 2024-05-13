53554e40e029f1ddd4f8c85ccb3d0fe2a7e06a28
reeffeer <Vfcnthgthytn1>
2024-05-13 19:25:48 +0300

    Added page home.html and MainController

---
+++ src/main/java/com/example/springbootdemo1/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
