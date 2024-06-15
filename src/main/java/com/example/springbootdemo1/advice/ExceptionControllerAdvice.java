5504f549094594847cf195ceae7da342a63219d5
reeffeer <Vfcnthgthytn1>
2024-06-15 22:54:24 +0300

    Exception handling with advice (AOP)

---
+++ src/main/java/com/example/springbootdemo1/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.example.springbootdemo1.advice;
+
+import com.example.springbootdemo1.exception.NotEnoughMoneyException;
+import com.example.springbootdemo1.model.ErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment.");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+}
